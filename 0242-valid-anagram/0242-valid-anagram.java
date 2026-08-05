4a504807f04168549a22ccdc766195400abb59be
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-08-05 12:43:21 +0530

    Time: 4 ms (82.68%), Space: 44.5 MB (60.56%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr=new int[26];
+        int[] empty=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            arr[t.charAt(i)-'a']--;
+        }
+        return Arrays.equals(arr,empty);
+    }
+}
