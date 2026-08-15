02b2b191062b69581605d41d7fa03f1e737956b6
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-08-15 13:07:19 +0530

    Time: 8 ms (96.09%), Space: 46.7 MB (84.31%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,29 +1,56 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> obj=new ArrayList<>();
-        if(s.length()<p.length()){
+        int n1=s.length();
+        int n2=p.length();
+        if(n1<n2){
             return obj;
         }
         int[] arr1=new int[26];
         int[] arr2=new int[26];
-        for(int i=0;i<p.length();i++){
+        for(int i=0;i<n2;i++){
             arr1[s.charAt(i)-'a']++;
         }
-        for(int i=0;i<p.length();i++){
+        for(int i=0;i<n2;i++){
             arr2[p.charAt(i)-'a']++;
         }
         if(Arrays.equals(arr1,arr2)){
             obj.add(0);
         }
-        int j=0;
-        for(int i=p.length();i<s.length();i++){
-            arr1[s.charAt(j)-'a']--;
-            j++;
+        for(int i=n2;i<n1;i++){
+            arr1[s.charAt(i-n2)-'a']--;
             arr1[s.charAt(i)-'a']++;
             if(Arrays.equals(arr1,arr2)){
-                obj.add(j);
+                obj.add(i-n2+1);
             }
         }
         return obj;
+        // List<Integer> obj=new ArrayList<>();
+        // int n1=s.length();
+        // int n2=p.length();
+        // if(n1<n2){
+        //     return obj;
+        // }
+        // int[] arr1=new int[26];
+        // int[] arr2=new int[26];
+        // for(int i=0;i<n2;i++){
+        //     arr1[s.charAt(i)-'a']++;
+        // }
+        // for(int i=0;i<n2;i++){
+        //     arr2[p.charAt(i)-'a']++;
+        // }
+        // if(Arrays.equals(arr1,arr2)){
+        //     obj.add(0);
+        // }
+        // int j=0;
+        // for(int i=n2;i<n1;i++){
+        //     arr1[s.charAt(j)-'a']--;
+        //     j++;
+        //     arr1[s.charAt(i)-'a']++;
+        //     if(Arrays.equals(arr1,arr2)){
+        //         obj.add(j);
+        //     }
+        // }
+        // return obj;
     }
 }
