f95dadea4504e48136a5307c01030912e63af571
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-08-18 20:18:08 +0530

    Time: 11 ms (96.61%), Space: 47.4 MB (42.69%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,5 +1,44 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words){
+        List<Integer> res=new ArrayList<>();
+        HashMap<String,Integer> obj=new HashMap<>();
+        for(String i:words){
+            obj.put(i,obj.getOrDefault(i,0)+1);
+        }
+        int len=words[0].length();
+        int n=words.length;
+        for(int i=0;i<len;i++){
+            int r=i;
+            int l=i;
+            int count=0;
+            HashMap<String,Integer> temp=new HashMap<>();
+            while(r+len<=s.length()){
+                String word=s.substring(r,r+len);
+                r+=len;
+                if(!obj.containsKey(word)){
+                    l=r;
+                    count=0;
+                    temp.clear();
+                    continue;
+                }
+                temp.put(word,temp.getOrDefault(word,0)+1);
+                count++;
+                while(temp.get(word)>obj.get(word)){
+                    String last=s.substring(l,l+len);
+                    temp.put(last,temp.get(last)-1);
+                    count--;
+                    l+=len;
+                }
+                if(count==n){
+                    res.add(l);
+                    String last=s.substring(l,l+len);
+                    temp.put(last,temp.get(last)-1);
+                    l+=len;
+                    count--;
+                }
+            }
+        }
+        return res;
         // List<Integer> res=new ArrayList<>();
         // HashMap<String,Integer> obj=new HashMap<>();
         // for(String i:words){
@@ -27,44 +66,5 @@ class Solution {
         //     }
         // }
         // return res;
-        List<Integer> res=new ArrayList<>();
-        int len=words[0].length();
-        int n=words.length;
-        HashMap<String,Integer> obj=new HashMap<>();
-        for(String i:words){
-            obj.put(i,obj.getOrDefault(i,0)+1);
-        }
-        for(int i=0;i<len;i++){
-            HashMap<String,Integer> temp=new HashMap<>();
-            int l=i;
-            int r=i;
-            int count=0;
-            while(r+len<=s.length()){
-                String word=s.substring(r,r+len);
-                r+=len;
-                if(!obj.containsKey(word)){
-                    l=r;
-                    temp.clear();
-                    count=0;
-                    continue;
-                }
-                temp.put(word,temp.getOrDefault(word,0)+1);
-                count++;
-                while(temp.get(word)>obj.get(word)){
-                    String last=s.substring(l,l+len);
-                    temp.put(last,temp.get(last)-1);
-                    l+=len;
-                    count--;
-                }
-                if(count==n){
-                    res.add(l);
-                    String last=s.substring(l,l+len);
-                    temp.put(last,temp.get(last)-1);
-                    l+=len;
-                    count--;
-                }
-            }
-        }
-        return res;
     }
 }
