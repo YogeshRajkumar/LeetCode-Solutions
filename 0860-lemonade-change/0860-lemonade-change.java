44a1883403a5abcfc2b3d8b6f1ca3e4fac61d84e
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-08-21 10:26:55 +0530

    Time: 2 ms (98%), Space: 72.4 MB (68.55%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean lemonadeChange(int[] arr) {
+        int five=0;
+        int ten=0;
+        for(int i:arr){
+            if(i==5){
+                five++;
+            }
+            else if(i==10){
+                if(five>=1){
+                    five--;
+                    ten++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(five>=1 && ten>=1){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five=five-3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
