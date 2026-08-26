27054810a41ab5ec6e518b96b3ba89ff00ffcb62
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-08-26 22:29:44 +0530

    Time: 0 ms (100%), Space: 41.9 MB (73.14%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return n;
+        }
+        int[] res=new int[n];
+        res[0]=1;
+        res[1]=2;
+        for(int i=2;i<n;i++){
+            res[i]=res[i-1]+res[i-2];
+        }
+        return res[n-1];
+        // int[] res=new int[n+1];
+        // res[1]=1;
+        // res[2]=2;
+        // for(int i=3;i<=n;i++){
+        //     res[i]=res[i-1]+res[i-2];
+        // }
+        // return res[n];
+    }
+}
