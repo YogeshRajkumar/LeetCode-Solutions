8e03a64dbd358c7f756cdc92f20ccffacf88f162
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-08-27 21:34:38 +0530

    Time: 3 ms (86.51%), Space: 52.4 MB (7%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int candy(int[] arr) {
+        int n=arr.length;
+        int[] res=new int[n];
+        res[0]=1;
+        for(int i=1;i<n;i++){
+            if(arr[i-1]<arr[i]){
+                res[i]=res[i-1]+1;
+            }
+            else{
+                res[i]=1;
+            }
+        }
+        int sum=0;
+        for(int i=n-1;i>0;i--){
+            if(arr[i-1]>arr[i]){
+                res[i-1]=Math.max(res[i-1],res[i]+1);
+            }
+            sum+=res[i];
+        }
+        return sum+=res[0];
+    }
+}
