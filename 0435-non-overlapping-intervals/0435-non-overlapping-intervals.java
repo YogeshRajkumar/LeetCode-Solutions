6a1ad3bb67c90e7b52afd99c4e5501a8d8638a33
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-09-08 10:13:42 +0530

    Update 0435-non-overlapping-intervals.java

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -10,5 +10,21 @@ class Solution {
             }
         }
         return arr.length-count;
+
+        // Arrays.sort(arr,(a,b)->a[0]-b[0]);
+        // int count=0;
+        // int prev=0;
+        // for(int i=1;i<arr.length;i++){
+        //     if(arr[prev][1]>arr[i][0]){
+        //         count++;
+        //         if(arr[prev][1]>arr[i][1]){
+        //             prev=i;
+        //         }
+        //     }
+        //     else{
+        //         prev=i;
+        //     }
+        // }
+        // return count;
     }
 }
