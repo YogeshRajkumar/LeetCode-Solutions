7a3086f710dec0ea719a714de2033585279d6b5c
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-09-09 12:35:35 +0530

    Time: 0 ms (100%), Space: 42.1 MB (91.24%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -8,5 +8,22 @@ class Solution {
             n=n>>1;
         }
         return count;
+
+        // int count=0;
+        // while(n!=0){
+        //     n=n & (n-1);
+        //     count++;
+        // }
+        // return count;
+
+        // String str=Integer.toBinaryString(n);
+        // int count=0;
+        // for(int i=0;i<str.length();i++){
+        //     if(str.charAt(i)=='1')
+        // {
+        //         count++;
+        //     }
+        // }
+        // return count;
     }
 }
