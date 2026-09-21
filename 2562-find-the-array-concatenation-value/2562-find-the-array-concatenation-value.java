c3aa1bfd054251c39decbddf4e39299f3cf48878
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-09-21 11:35:12 +0530

    Time: 1 ms (93.72%), Space: 44.6 MB (89.09%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
@@ -18,5 +18,19 @@ class Solution {
             res+=arr[l];
         }
         return res;
+
+        // int l=0;
+        // int r=arr.length-1;
+        // long res=0;
+        // while(l<r){
+        //     String s1=""+arr[l]+arr[r];
+        //     res+=Integer.parseInt(s1);
+        //     l++;
+        //     r--;
+        // }
+        // if(arr.length%2!=0){
+        //     res+=arr[l];
+        // }
+        // return res;
     }
 }
