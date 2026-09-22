dda9a488c3f86f5aa1cc2523c8033d8713ec6deb
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-09-22 09:46:15 +0530

    Time: 954 ms (88.13%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,8 @@
+select contest_id,
+    round(
+        (count(*)/(select count(*) from users))*100,
+        2
+    ) as percentage
+from Register
+group by contest_id
+order by percentage desc,contest_id asc;
