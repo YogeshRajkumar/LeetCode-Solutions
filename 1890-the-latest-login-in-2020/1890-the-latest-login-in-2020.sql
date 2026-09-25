2604450144b0e5a0bba6c0d6ce11c5bbc52cb670
Yogesh Rajkumar <yogeshraj.btech@gmail.com>
2026-09-25 06:35:06 +0530

    Update 1890-the-latest-login-in-2020.sql

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,3 +1,3 @@
 select user_id,max(time_stamp) as last_stamp from logins
-where time_stamp>='2020-01-01' and time_stamp<'2021-01-01'
+where '2020-01-01'<=time_stamp and time_stamp<'2021-01-01'
 group by user_id;
